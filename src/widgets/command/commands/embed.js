866f329fb5b55d46b3d423d40bd5e31d6674fee8
saptarshibasu15 <123saptarshi.basu@gmail.com>
2021-03-24 23:49:21 +0530

    embed added

---
+++ src/widgets/command/commands/embed.js
@@ -0,0 +1,20 @@
+const CommandBuilder = require("../classes/CommandBuilder");
+const {MessageEmbed}=require("discord.js");
+
+module.exports = new CommandBuilder()
+  .setAliases(["embed"])
+  .setOwnersOnly(false)
+  .setGuildOnly(false)
+  .setRequireArgs(false)
+  .setDeletable(false)
+  .setCooldown(10)
+  .setDisabled(false)
+  // eslint-disable-next-line
+  .setExecute(async (message, user, args) => {
+    let embed=new MessageEmbed()
+      .setAuthor(`${message.author.username}`,`${message.author.avatarURL()}`)
+      .setDescription(`${args.join(" ")}`)
+      .setColor("#d6a844")
+    message.delete();
+    message.channel.send(embed);
+  });
