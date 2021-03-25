852e700eae08c364f77c1053549d78494dcd683b
saptarshibasu15 <123saptarshi.basu@gmail.com>
2021-03-25 14:20:54 +0530

    server onboard

--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@ require("dotenv").config();
 const { Client } = require("discord.js");
 
 const client = new Client();
+const express = require("express");
+
+client.on("message", (msg) => {
+  client.user.setActivity("with Cats", {
+    type: "PLAYING",
+  });
+});
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
 
 require("./core/loadWidgetListeners")(client);
 
