cfe3f601948878d5cf91d45f402ce8d942559857
saptarshibasu15 <123saptarshi.basu@gmail.com>
2021-03-25 14:30:49 +0530

    server onboard

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,23 @@
-import dotenv from "dotenv"; 
+import dotenv from "dotenv";
 dotenv.config();
 import { Client } from "discord.js";
 
-const client = new Client();
+const express = require("express");
+const client=new Client();
+
+client.on("message", (msg) => {
+  client.user.setActivity("with Cats", {
+    type: "PLAYING",
+  });
+});
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
+
 
 require("./core/loadWidgetListeners")(client);
 
