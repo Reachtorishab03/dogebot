b560392907f99c3d94838de7932d4dbb2a05bf6c
Replit user <replituser@example.com>
2021-03-27 07:46:47 +0000

    fixed av

--- src/widgets/command/commands/avatar.ts
+++ src/widgets/command/commands/avatar.ts
@@ -13,18 +13,20 @@ module.exports = new CommandBuilder()
   // eslint-disable-next-line
   .setExecute(async (message, user, args) => {
     if(args.length==0){
+      console.log(message.author)
       let embed=new MessageEmbed()
         .setAuthor(`${message.author.username}`,`${message.author.avatarURL()}`)
-        .setImage(`${message.author.avatarURL()}`)
+        .setImage(`https://cdn.discordapp.com/avatars/${message.author.id}/${message.author.avatar}.png?size=1024`)
         .setColor("#d6a844")
       message.channel.send(embed);
     }
     if(args.length>0){
-      console.log(args[0],args[0].substring(2,(args[0].length)-2))
-      const usr=client.users.cache.get(args[0].substring(2,(args[0].length)-1))
+      console.log(args[0],args[0].substring(3,(args[0].length)-1))
+      const usr=client.users.cache.get(args[0].substring(3,(args[0].length)-1))
+      console.log(usr);
       let embed=new MessageEmbed()
         .setAuthor(`${usr.username}`,`${usr.displayAvatarURL()}`)
-        .setImage(`${usr.displayAvatarURL()}`)
+        .setImage(`https://cdn.discordapp.com/avatars/${usr.id}/${usr.avatar}.png?size=1024`)
         .setColor("#d6a844")
       message.channel.send(embed);
     }
